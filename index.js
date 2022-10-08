c5297dacbe464cf94cb2ffa4e14d716e7f729a08
aayush2712 <34811105+aayush2712@users.noreply.github.com>
2022-10-08 15:42:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let counter = document.getElementById('home-point-text');
+let counter1 = document.getElementById('guest-point-text');
+
+function homeIncrement1() {
+  counter.innerText++;
+}
+
+function homeIncrement2() {
+  counter.innerText++;
+  counter.innerText++;
+}
+
+function homeIncrement3() {
+  counter.innerText++;
+  counter.innerText++;
+  counter.innerText++;
+}
+
+function guestIncrement1() {
+  counter1.innerText++;
+}
+
+function guestIncrement2() {
+  counter1.innerText++;
+  counter1.innerText++;
+}
+
+function guestIncrement3() {
+  counter1.innerText++;
+  counter1.innerText++;
+  counter1.innerText++;
+}
